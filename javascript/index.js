27f89653483e028d1b00bfc4babffd5c205f7726
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-10-24 00:02:17 +0300

    product and deal have been finished successfully

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
-console.log("from js");
+// preview =>// https://preview.colorlib.com/theme/product/index.html
+const productCont = document.querySelector("#products > .container");
+// console.log(productCont);
+// const products = [];
+// fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((json) => {
+//     console.log(json);
+//     products.push(json); 
+    
+// const createWeatherWeekly = (element) => {
+//     return `
+//     <div class="product col-6 col-sm-6 col-md-6 mb-4 col-lg-4 px-2">
+//     <div class="image">
+//       <span class="badge">NEW</span>
+//       <img class="product-image w-100" src=${element.image}  alt="Product Image">
+//     </div>
+//     <div class="product-info text text-center">
+//       <h6 class="my-1 product-title">${element.title.slice(0,10)}...</h6>
+//       <p class="product-price">£${element.price}</p>
+//     </div>
+//   </div>
+//   `;
+//   };
+//   json?.forEach((element) => {
+//     productCont.insertAdjacentHTML("beforeend", createWeatherWeekly(element));
+//   });
+    
+//   });
+// console.log(products);
+
