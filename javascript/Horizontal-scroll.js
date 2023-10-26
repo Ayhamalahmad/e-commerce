e22847868d26a4ccd358f6cac0e1d47acde66253
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-10-26 20:48:26 +0300

    .

---
+++ javascript/Horizontal-scroll.js
@@ -0,0 +1,10 @@
+// const previous =document.querySelector(".previous");
+// const nextBtn =document.querySelector(".nextBtn");
+// const PopularIGallery =document.querySelector(".Popular-Items .gallery");
+// const PopularICarouselItem =document.querySelector("#Popular-Items .carousel-inner .carousel-item");
+// console.log(PopularICarouselItem);
+// console.log(PopularICarouselItem.offsetWidth);
+// previous.addEventListener("click",(e)=>{
+//     PopularIGallery.scrollLeft += PopularICarouselItem.offsetWidth;
+//     console.log("scrollLeft");
+// })
