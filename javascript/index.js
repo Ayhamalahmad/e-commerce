560118137457bca37e08d21534070a16f3fb57be
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-10-31 23:19:15 +0300

    scrolled

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
 // preview =>// https://preview.colorlib.com/theme/product/index.html
+const navbar = document.querySelector(".navbar");
+const scrollThreshold = 100;
+
+window.addEventListener("scroll", (e) => {
+  const scrollTop = window.scrollY;
+
+  if (scrollTop >= scrollThreshold) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
